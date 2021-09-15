a7ac83267c41df60175fb706920b74be575d1392
hanasai95 <1220hanasai@gmail.com>
2021-09-14 23:15:52 -0400

    add a link between home page and post page(fixed)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="title">
+        <h1 className={styles.title}>
           Read{' '}
           <Link href="/posts/first-post">
             <a>this page!</a>
